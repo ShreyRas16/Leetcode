110e7d6ef4dbdeb7e744a9ebeae140202a20f5e0
ShreyRas16 <rastogi.shrey16@gmail.com>
2026-07-24 10:01:56 +0530

    Time: 0 ms (100%), Space: 31.2 MB (74.4%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,19 +11,24 @@
  */
 class Solution {
 public:
-    unordered_map<int,vector<TreeNode*>> mp;
-    void count(TreeNode* root,int level){
-        if(root==NULL) return;
-        level++;
-        mp[level].push_back(root);
-        count(root->left,level);
-        count(root->right,level);
+    int lefth(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+lefth(root->left);
+    }
+    int righth(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+righth(root->right);
+    }
+    int sum=0;
+    int count(TreeNode* root){
+        if(root==NULL) return 0;
+        if(lefth(root)==righth(root)) return (1<<lefth(root))-1;
+        int l=count(root->left);
+        int r=count(root->right);
+        return 1+l+r;
     }
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
-        count(root,0);
-        vector<vector<TreeNode*>> ans;
-        for(int i=1;i<mp.size()+1;i++) ans.push_back(mp[i]);
-        return (1<<(ans.size()-1))-1+ans[ans.size()-1].size();
+        return count(root);
     }
 };
