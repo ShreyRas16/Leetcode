ca2cad081525deaacb2c454421cb349123978682
ShreyRas16 <rastogi.shrey16@gmail.com>
2026-08-10 10:04:22 +0530

    Time: 3 ms (75.09%), Space: 20 MB (38.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    bool check(int node,vector<vector<int>>& adj,vector<bool>& vis,vector<bool>& path){
-        vis[node]=true;
-        path[node]=true;
-        for(int next:adj[node]){
-            if(!vis[next]){
-                if(check(next,adj,vis,path)) return true;
-            }
-            else if(path[next]) return true;
-        }
-        path[node]=false;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,int> indegree;
         vector<vector<int>> adj(numCourses);
         for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
-        vector<bool> vis(numCourses);
-        vector<bool> path(numCourses);
+        for(int i=0;i<adj.size();i++){
+            for(int j=0;j<adj[i].size();j++) indegree[adj[i][j]]++;
+        }
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(check(i,adj,vis,path)) return false;
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int temp=q.front();
+            q.pop();
+            topo.push_back(temp);
+            for(int i=0;i<adj[temp].size();i++){
+                indegree[adj[temp][i]]--;
+                if(indegree[adj[temp][i]]==0) q.push(adj[temp][i]);
             }
         }
+        if(topo.size()!=numCourses) return false;
         return true;
     }
 };
