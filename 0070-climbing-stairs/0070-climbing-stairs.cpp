2deef76b3235de42b31cc09cdebdd85aee778885
ShreyRas16 <rastogi.shrey16@gmail.com>
2026-08-28 22:54:21 +0530

    Time: 2 ms (5.11%), Space: 8.6 MB (39.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int find(vector<int>& dp,int n){
-        if(n<0) return 0;
-        if(n==0) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=find(dp,n-1)+find(dp,n-2);
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return find(dp,n);
+    if (n<=2) return n;
+    vector<int> dp(n+1);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i=3;i<=n;i++) {
+        dp[i]=dp[i-1]+dp[i-2];
     }
+    return dp[n];
+}
 };
