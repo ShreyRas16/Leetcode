6ec9be4f0207a95d3a87f8c5c36c3df248c8860c
ShreyRas16 <rastogi.shrey16@gmail.com>
2026-09-01 10:24:58 +0530

    Time: 0 ms (100%), Space: 10.9 MB (6.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    int find(int idx,vector<int>& dp,vector<int>& nums){
-        if(idx>nums.size()-1) return 0;
-        if(idx==nums.size()-1) return dp[nums.size()-1];
-        if(idx==nums.size()-2) return dp[nums.size()-2];
-        if(dp[idx]!=-1) return dp[idx];
-        int maxi=INT_MIN;
-        for(int i=idx+2;i<nums.size();i++){
-            if(dp[i]==-1) find(i,dp,nums);
-            maxi=max(maxi,dp[i]);
-        }
-        return dp[idx]=maxi+nums[idx];
-    }
     int rob(vector<int>& nums) {
         if(nums.size()==1) return nums[0];
-        vector<int> dp(nums.size(),-1);
+        vector<int> dp(nums.size());
         dp[nums.size()-1]=nums[nums.size()-1];
-        dp[nums.size()-2]=nums[nums.size()-2];
-        return max(find(0,dp,nums),find(1,dp,nums));
+        dp[nums.size()-2]=max(nums[nums.size()-1],nums[nums.size()-2]);
+        if(nums.size()==2) return max(dp[0],dp[1]);
+        for(int i=nums.size()-3;i>=0;i--){
+            dp[i]=max(dp[i+2]+nums[i],dp[i+1]);
+        }
+        return max(dp[0],dp[1]);
     }
 };
