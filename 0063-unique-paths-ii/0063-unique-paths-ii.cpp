3ec94a478c93a683144bd778b4c21e4c8e2756ee
ShreyRas16 <rastogi.shrey16@gmail.com>
2026-09-07 04:18:04 +0530

    Time: 0 ms (100%), Space: 12 MB (6.51%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int paths(vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp,int i,int j){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==0 && j==0) return dp[i][j]=1;
-        if(obstacleGrid[i][j]==1) return dp[i][j]=0;
-        int up=0;
-        int left=0;
-        if(i>0) up=paths(obstacleGrid,dp,i-1,j);
-        if(j>0) left=paths(obstacleGrid,dp,i,j-1);
-        return dp[i][j]=up+left;
-    }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        vector<vector<int>> dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),-1));
         if(obstacleGrid[0][0]==1 || obstacleGrid[obstacleGrid.size()-1][obstacleGrid[0].size()-1]==1) return 0;
-        return paths(obstacleGrid,dp,obstacleGrid.size()-1,obstacleGrid[0].size()-1);
+        vector<vector<int>> dp(obstacleGrid.size(),vector<int>(obstacleGrid[0].size(),-1));
+        dp[0][0]=1;
+        for(int i=0;i<obstacleGrid.size();i++){
+            for(int j=0;j<obstacleGrid[0].size();j++){
+                if(i==0 && j==0) continue;
+                dp[i][j]=0;
+                if(obstacleGrid[i][j]==1) continue;
+                if(i>0 && obstacleGrid[i-1][j]!=1) dp[i][j]=dp[i][j]+dp[i-1][j];
+                if(j>0 && obstacleGrid[i][j-1]!=1) dp[i][j]=dp[i][j]+dp[i][j-1];
+            }
+        }
+        return dp[obstacleGrid.size()-1][obstacleGrid[0].size()-1];
     }
 };
