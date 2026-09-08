75ff366559127bf32a6d7d7f95b05bcf1225178e
ShreyRas16 <rastogi.shrey16@gmail.com>
2026-09-08 10:30:07 +0530

    Time: 0 ms (100%), Space: 13.4 MB (45.02%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int sum(vector<vector<int>>& triangle,vector<vector<int>>& dp,int i,int j){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==0 && j==0) return dp[i][j]=triangle[i][j];
-        int curr=INT_MAX;
-        int bef=INT_MAX;
-        if(i>0 && j>0) bef=sum(triangle,dp,i-1,j-1);
-        if(i>0 && j<triangle[i-1].size()) curr=sum(triangle,dp,i-1,j);
-        return dp[i][j]=triangle[i][j]+min(bef,curr);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>> dp(triangle.size(),vector<int>(triangle[triangle.size()-1].size(),-1));
+        vector<vector<int>> dp(triangle.size(),vector<int>(triangle[triangle.size()-1].size(),0));
+        dp[0][0]=triangle[0][0];
         int mini=INT_MAX;
+        for(int i=0;i<triangle.size();i++){
+            for(int j=0;j<triangle[i].size();j++){
+                int curr=INT_MAX;
+                int bef=INT_MAX;
+                if(i==0 && j==0) continue;
+                if(i>0 && j>0) bef=dp[i-1][j-1];
+                if(i>0 && j<triangle[i-1].size()) curr=dp[i-1][j];
+                dp[i][j]=triangle[i][j]+min(curr,bef);
+            }
+        }
         for(int i=0;i<triangle[triangle.size()-1].size();i++){
-            mini=min(mini,sum(triangle,dp,triangle[triangle.size()-1].size()-1,i));
+            mini=min(dp[triangle.size()-1][i],mini);
         }
         return mini;
     }
